9d379e0b10cbe01edd4dbd3e7e4645e45b99a75c
hui <125173679@qq.com>
2022-02-17 18:38:00 +0800

    init

---
+++ app.js
@@ -0,0 +1,45 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+
+http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url)
+  if (pathname === '/') {
+    const data = fs.readFileSync('./index.html')
+    res.end(data)
+  } else if (pathname === '/images/01.jpeg') {
+    const data = fs.readFileSync('./images/01.jpeg')
+
+    // expires
+    // res.writeHead(200, {
+    //   // 2022-02-17 17:48:45
+    //   Expires: new Date('2022-02-17 17:50:30').toUTCString()
+    // })
+
+    // cache-control: max-age=5
+    // res.writeHead(200, {
+    //   'Cache-control': 'max-age=5' // 资源在5s后过期
+    // })
+
+    // last-modified
+    // 文件修改时间
+    const { mtime } = fs.statSync('./images/01.jpeg')
+    const ifModifiedSince = req.headers['if-modified-since']
+    // 如果文件修改时间等于 `if-modified-since` 时间，说明资源没有被修改过可以直接返回304状态码
+    if (ifModifiedSince === mtime.toUTCString()) {
+      res.statusCode = 304
+      res.end()
+      return
+    }
+    // 文件发生了修改或者第一次请求资源
+    res.setHeader('last-modified', mtime.toUTCString())
+    res.setHeader('Cache-Control', 'no-cache')
+    
+    res.end(data)
+  } else {
+    res.statusCode = 404
+    res.end()
+  }
+}).listen(3000, () => {
+  console.log('http://localhost:3000')
+})
